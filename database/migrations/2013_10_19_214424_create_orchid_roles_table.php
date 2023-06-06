7b20443505bb003c29cd587dfc0e10d2f2183831
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-06 16:06:29 +0200

    add column slug to roles

--- database/migrations/2013_10_19_214424_create_orchid_roles_table.php
+++ database/migrations/2013_10_19_214424_create_orchid_roles_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table): void {
             $table->increments('id');
-            $table->string('name_of')->unique();
+            $table->string('name_of');
+            $table->string('slug')->unique();
             $table->string('misc')->nullable();
             $table->boolean('is_active');
             $table->timestamps();
